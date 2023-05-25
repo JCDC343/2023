984ccbf3f589646604cc5d3faf41182c1f6b3ce0
JCDC343 <diaz.j@maristcollege.school.nz>
2023-05-25 13:03:37 +1200

    Changed "back to top" button name

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //Scroll back to top of page
 // Get the button
-let mybutton = document.getElementById("myBtn");
+let btpButton = document.getElementById("btp-btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    btpButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    btpButton.style.display = "none";
   }
 }
 
